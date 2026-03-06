60c2a7bf015ca1c7f44ebd3441103aac3bc6a131
Bhavishya-Bansal <bhavishya.bansal0207@gmail.com>
2026-03-06 22:55:18 +0530

    Modified after Cloning

--- Launch.java
+++ Launch.java
@@ -2,4 +2,9 @@ class Launch{
 	public static void main(String[] args){
 		System.out.println("Hello GitHub");		
 	}
+	public static void add(){
+		int a = 11;
+		int b = 21;
+		System.out.println(a+b);
+	}
 }
